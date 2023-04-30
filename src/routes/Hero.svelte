cd5825c5f25089b2f9e7aea32dab92a3bf2e0d49
Richard <richard.verheyen@gmail.com>
2023-04-30 10:44:57 +1000

    Added logo to hero

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,9 +1,16 @@
 <script>
+  import logo from "$lib/images/logo.svg";
 </script>
 
 <section id="top">
   <div class="gutters">
-    
+    <picture>
+      <source
+        type="image/webp"
+        srcset={logo}
+      />
+      <img id="hero-logo" src={logo} width=400 height=300 alt="Floatplane">
+    </picture>
   </div>
 </section>
 
@@ -19,13 +26,23 @@
     overflow: hidden;
     background: linear-gradient(#20717D, #569F9E, #E7C29F);
 
-    > div.gutter {
+    > div.gutters {
       position: relative;
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
       width: 100%;
+
+      #hero-logo {
+        padding-right: 50px;
+
+        @media (max-width: 500px) {
+          width: 240px;
+          height: 200px;
+          padding-right: 30px;
+        }
+      }
     }
   }
 </style>
